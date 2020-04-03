322e101dc87ef120e460bf5f47ab5dd46200046a
Drew Ardner <KyomamruS@gmail.com>
2020-04-02 22:03:26 -0400

    Preliminary graphics setup for UI elements.

---
+++ build/LargeNumberSprites.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(LargeNumberSprites)
+
+//======================================================================
+//
+//	LargeNumberSprites, 16x352@4, 
+//	Transparent color : FF,00,BB
+//	+ palette 2 entries, not compressed
+//	+ 88 tiles Metatiled by 2x4 not compressed
+//	Total size: 4 + 2816 = 2820
+//
+//	Time-stamp: 2020-04-02, 20:42:48
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_LARGENUMBERSPRITES_H
+#define GRIT_LARGENUMBERSPRITES_H
+
+#define LargeNumberSpritesTilesLen 2816
+extern const unsigned int LargeNumberSpritesTiles[704];
+
+#define LargeNumberSpritesPalLen 4
+extern const unsigned short LargeNumberSpritesPal[2];
+
+#endif // GRIT_LARGENUMBERSPRITES_H
+
+//}}BLOCK(LargeNumberSprites)
---
+++ build/SmallFontSprites.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(SmallFontSprites)
+
+//======================================================================
+//
+//	SmallFontSprites, 16x592@4, 
+//	Transparent color : FF,00,BB
+//	+ palette 2 entries, not compressed
+//	+ 148 tiles Metatiled by 1x2 not compressed
+//	Total size: 4 + 4736 = 4740
+//
+//	Time-stamp: 2020-04-02, 20:42:48
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SMALLFONTSPRITES_H
+#define GRIT_SMALLFONTSPRITES_H
+
+#define SmallFontSpritesTilesLen 4736
+extern const unsigned int SmallFontSpritesTiles[1184];
+
+#define SmallFontSpritesPalLen 4
+extern const unsigned short SmallFontSpritesPal[2];
+
+#endif // GRIT_SMALLFONTSPRITES_H
+
+//}}BLOCK(SmallFontSprites)
